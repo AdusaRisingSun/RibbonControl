f4e87e9cb25e806d2884b1a964b0e92d5f1158ce
AdusaRisingSun <Adusa@AdusadeMac-mini.local>
2015-09-06 09:18:47 +0800

    Initial Commit

---
+++ RibbonControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RibbonControl
+//
+//  Created by Adusa on 15/9/6.
+//  Copyright (c) 2015年 Adusa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
