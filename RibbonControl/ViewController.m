f77deb81267ae2b9a2a01657e3ec643cf6c2c89f
AdusaRisingSun <Adusa@AdusadeMac-mini.local>
2015-09-07 17:36:33 +0800

    change sth in ViewController

--- RibbonControl/ViewController.m
+++ RibbonControl/ViewController.m
@@ -28,24 +28,27 @@ const CGFloat kDesiredHeight=120.0f;
     [super viewDidLoad];
     isHidden=YES;
     hiddenView=[[UIView  alloc]init];
-    hiddenView.backgroundColor=[[UIColor lightGrayColor]colorWithAlphaComponent:0.5f];
+    hiddenView.backgroundColor=[UIColor greenColor];
+    //[hiddenView setBounds:CGRectMake(0, 0, self.view.bounds.size.width, 120.0)];
+    [self.view addSubview:hiddenView];
     ribbonPull=[[RibbonPull alloc]init];
+    [self.view addSubview:ribbonPull];
     
     [hiddenView setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:hiddenView attribute: NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeLeft multiplier:1.0f constant:0.0f]];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:hiddenView attribute: NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeRight multiplier:1.0f constant:0.0f]];
-    [hiddenView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:(@"V:|-[hiddenView(==120.0)]") options:0 metrics:nil views:NSDictionaryOfVariableBindings(hiddenView)]];
+    
+    [hiddenView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:(@"V:[hiddenView(==120.0)]") options:0 metrics:nil views:NSDictionaryOfVariableBindings(hiddenView)]];
     hiddentViewTopConstraint =[NSLayoutConstraint constraintWithItem:hiddenView attribute: NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0f constant:-kDesiredHeight];
     [self.view addConstraint:hiddentViewTopConstraint];
+    
     [ribbonPull setTranslatesAutoresizingMaskIntoConstraints:NO];
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:ribbonPull attribute: NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeRight multiplier:1.0f constant:-30]];
     ribbonPullTopConstraint =[NSLayoutConstraint constraintWithItem:ribbonPull attribute: NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeTop multiplier:1.0f constant:0];
      [self.view addConstraint:ribbonPullTopConstraint];
-    
-    [self.view addSubview:hiddenView];
-    [self.view addSubview:ribbonPull];
+ 
     [ribbonPull addTarget:self action:@selector(ribbonPull:) forControlEvents:UIControlEventValueChanged];
-    self.edgesForExtendedLayout=UIRectEdgeNone;
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
